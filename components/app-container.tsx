b04e0ac633e5dd411030521c0e1fc83db2ec8c58
Tim Copland <tim.copland@august.com.au>
2021-12-25 09:43:56 +1100

    Actually fix typescript?

--- components/app-container.tsx
+++ components/app-container.tsx
@@ -4,7 +4,7 @@ import utilStyles from './utils.module.scss'
 import classnames from 'classnames'
 
 
-export default function AppContainer({children, home, background}: {children: Element[], home: boolean, background: string} ) {
+export default function AppContainer({children, home, background}: {children?: React.ReactChild, home: boolean, background?: string} ) {
     const cn = classnames( {
         [styles.container]: true,
         [styles.red]: background === 'red'
